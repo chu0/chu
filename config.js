ee37c2b9d75d7084b3ce728c7522d0191b9b4b7e
chu0 <chu.china@hotmail.com>
2016-06-04 13:44:55 +0800

    first commit

---
+++ config.js
@@ -0,0 +1,12 @@
+config = {
+    blogname: 'chu',
+    sep: ' | ',
+    user: 'chu0',
+    repo: 'chu',
+    per_page: 1,
+    duoshuo_id: '*'
+}
+
+var duoshuoQuery = {
+    short_name: config['duoshuo_id']
+};
